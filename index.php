07359c067bf968ccaa6866e8a930a9a4d96d7b01
Windram <nillorcool@gmail.com>
2019-09-22 17:48:46 +0200

    starting over with sessions

--- index.php
+++ index.php
@@ -1,50 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
+session_start();
 
-$number= $_POST['number_entered'];
+if (!isset($_POST["guess"])) {
+     $_SESSION["count"] = 0; //Initialize count
+     $message = "Welcome to the guessing machine!";
+     $_POST["numtobeguessed"] = rand(0,10);
+    
+} else if ($_POST["guess"] > $_POST["numtobeguessed"]) { //greater than
+    $message = $_POST["guess"]." is too big! Try a smaller number.";
+    $_SESSION["count"]++; //Declare the variable $count to increment by 1.
 
-$submitbutton= $_POST['submit'];
+} else if ($_POST["guess"] < $_POST["numtobeguessed"]) { //less than
+    $message = $_POST["guess"]." is too small! Try a larger number.";
+    $_SESSION["count"]++; //Declare the variable $count to increment by 1.
 
-
-$randomnumber= mt_rand(1,10);
-
-
+} else { // must be equivalent
+    $_SESSION["count"]++;
+    $message = "Well done! You guessed the right number in ".$_SESSION["count"]." attempt(s)!"; 
+    unset($_SESSION["count"]);
+        //Include the $count variable to the $message to show the user how many tries to took him.
+}
 ?>
+<html>
 
-
-<form action="" method="POST">
-Guess a Number Between 1 and 10: 
-<input type="number" name="number_entered" min="1" max="10"/> <br><br>
-
-Result: 
-<?php 
-if ($submitbutton){
-
-if (($number > 0) && ($number <11)){
-if ($number != $randomnumber)
-{
-echo "Incorrect guess. The correct number was $randomnumber. Try again";
-}
-else 
-{
-echo "$randomnumber is the correct guess. You got it right.";
-}
-}
-
-}
-
-?>
-<br><br>
-<input type="submit" name="submit" value="Search"/><br><br>
-</form>
-
-</body>
+    <head>
+        <title>A PHP number guessing script</title>
+    </head>
+    <body>
+    <h1><?php echo $message; ?></h1>
+        <form action="" method="POST">
+        <p><strong>Type your guess here:</strong>
+            <input type="text" name="guess"></p>
+            <input type="hidden" name="numtobeguessed" 
+                   value="<?php echo $_POST["numtobeguessed"]; ?>" ></p>
+    <p><input type="submit" value="Submit your guess"/></p>
+        </form>
+    </body>
 </html>
